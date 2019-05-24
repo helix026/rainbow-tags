37799535295dc277494a244a597047d9607e2487
voldemortensen <voldemortensen@users.noreply.github.com>
2019-05-24 13:59:44 -0600

    use prettier, add color config options

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 Set rainbow colors for HTML div tags. This makes it easier to find matching divs.
 
 ![Example](example.png)
+
+## Configuration
+
+You can set `rainbowTags.colors` in settings to customize what colors are used for highlighting.
+
+Example:
+```javascript
+{
+  "rainbowTags.colors": ["#d26", "red", "rgba(100, 200, 100, 0.5)"]
+}
+```
--- extension.js
+++ extension.js
@@ -1,96 +1,129 @@
-const vscode = require('vscode');
-const divsColor = ["#e6b422", "#c70067", "#00a960", "#fc7482"];
-const isolatedRightBracketsDecorationTypes = vscode.window.createTextEditorDecorationType({
+const vscode = require("vscode");
+const divsColor = vscode.workspace
+  .getConfiguration("rainbowTags")
+  .get("colors");
+const isolatedRightBracketsDecorationTypes = vscode.window.createTextEditorDecorationType(
+  {
     color: "#e2041b"
-});
+  }
+);
 let divsDecorationTypes = [];
 
 function activate(context) {
-    for (var index in divsColor) {
-        divsDecorationTypes.push(vscode.window.createTextEditorDecorationType({
-            color: divsColor[index]
-        }));
+  for (var index in divsColor) {
+    divsDecorationTypes.push(
+      vscode.window.createTextEditorDecorationType({
+        color: divsColor[index]
+      })
+    );
+  }
+
+  let disposable = vscode.commands.registerCommand(
+    "extension.rainbowTags",
+    () => {
+      rainbowTags(vscode.window.activeTextEditor);
     }
+  );
+  context.subscriptions.push(disposable);
 
-    let disposable = vscode.commands.registerCommand('extension.rainbowTags', () => {
-        rainbowTags(vscode.window.activeTextEditor);
-    });
-    context.subscriptions.push(disposable);
+  vscode.window.onDidChangeActiveTextEditor(
+    editor => {
+      rainbowTags(editor);
+    },
+    null,
+    context.subscriptions
+  );
 
-    vscode.window.onDidChangeActiveTextEditor((editor) => {
-        rainbowTags(editor);
-    }, null, context.subscriptions);
-
-    vscode.workspace.onDidChangeTextDocument(function (event) {
-        var activeEditor = vscode.window.activeTextEditor;
-        if (activeEditor && event.document === activeEditor.document) {
-            rainbowTags(activeEditor);
-        }
-    }, null, context.subscriptions);
-
+  vscode.workspace.onDidChangeTextDocument(
+    function(event) {
+      var activeEditor = vscode.window.activeTextEditor;
+      if (activeEditor && event.document === activeEditor.document) {
+        rainbowTags(activeEditor);
+      }
+    },
+    null,
+    context.subscriptions
+  );
 }
 
 function rainbowTags(activeEditor) {
-    if (!activeEditor) {
-        return;
-    }
+  if (!activeEditor) {
+    return;
+  }
 
-    var text = activeEditor.document.getText();
-    var regEx = /(<\/?div(?:[^>]*)?>)/g;
-    var match;
-    var divsColorCount = 0;
-    var openDivStack = [];
-    var divsDecorationTypeMap = {};
+  var text = activeEditor.document.getText();
+  var regEx = /(<\/?div(?:[^>]*)?>)/g;
+  var match;
+  var divsColorCount = 0;
+  var openDivStack = [];
+  var divsDecorationTypeMap = {};
 
-    for (var index in divsDecorationTypes) {
-        divsDecorationTypeMap[index] = [];
-    }
-    ;
+  for (var index in divsDecorationTypes) {
+    divsDecorationTypeMap[index] = [];
+  }
 
-    var rightBracketsDecorationTypes = [];
-    var roundCalculate;
-    while (match = regEx.exec(text)) {
-        switch (match[0].substring(0, 2)) {
-            case '<d':
-                var startPos = activeEditor.document.positionAt(match.index);
-                var endPos = activeEditor.document.positionAt(match.index + 5);
-                var closeTagStartPos = activeEditor.document.positionAt(regEx.lastIndex - 1);
-                var closeTagEndPos = activeEditor.document.positionAt(regEx.lastIndex);
-                var decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: null };
-                var closeTagDecoration = { range: new vscode.Range(closeTagStartPos, closeTagEndPos), hoverMessage: null };
-                roundCalculate = divsColorCount;
-                openDivStack.push(roundCalculate);
-                divsColorCount++;
-                if (divsColorCount >= divsColor.length) {
-                    divsColorCount = 0;
-                }
-                divsDecorationTypeMap[roundCalculate].push(decoration);
-                divsDecorationTypeMap[roundCalculate].push(closeTagDecoration);
-                break;
-            case '</':
-                var startPos = activeEditor.document.positionAt(match.index);
-                var endPos = activeEditor.document.positionAt(match.index + 6);
-                var decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: null };
-                if (openDivStack.length > 0) {
-                    roundCalculate = openDivStack.pop();
-                    divsColorCount = roundCalculate;
-                    divsDecorationTypeMap[roundCalculate].push(decoration);
-                } else {
-                    rightBracketsDecorationTypes.push(decoration);
-                }
-                break;
-            default:
+  var rightBracketsDecorationTypes = [];
+  var roundCalculate;
+  while ((match = regEx.exec(text))) {
+    switch (match[0].substring(0, 2)) {
+      case "<d":
+        var startPos = activeEditor.document.positionAt(match.index);
+        var endPos = activeEditor.document.positionAt(match.index + 5);
+        var closeTagStartPos = activeEditor.document.positionAt(
+          regEx.lastIndex - 1
+        );
+        var closeTagEndPos = activeEditor.document.positionAt(regEx.lastIndex);
+        var decoration = {
+          range: new vscode.Range(startPos, endPos),
+          hoverMessage: null
+        };
+        var closeTagDecoration = {
+          range: new vscode.Range(closeTagStartPos, closeTagEndPos),
+          hoverMessage: null
+        };
+        roundCalculate = divsColorCount;
+        openDivStack.push(roundCalculate);
+        divsColorCount++;
+        if (divsColorCount >= divsColor.length) {
+          divsColorCount = 0;
         }
+        divsDecorationTypeMap[roundCalculate].push(decoration);
+        divsDecorationTypeMap[roundCalculate].push(closeTagDecoration);
+        break;
+      case "</":
+        var startPos = activeEditor.document.positionAt(match.index);
+        var endPos = activeEditor.document.positionAt(match.index + 6);
+        var decoration = {
+          range: new vscode.Range(startPos, endPos),
+          hoverMessage: null
+        };
+        if (openDivStack.length > 0) {
+          roundCalculate = openDivStack.pop();
+          divsColorCount = roundCalculate;
+          divsDecorationTypeMap[roundCalculate].push(decoration);
+        } else {
+          rightBracketsDecorationTypes.push(decoration);
+        }
+        break;
+      default:
     }
-    for (var index in divsDecorationTypes) {
-        activeEditor.setDecorations(divsDecorationTypes[index], divsDecorationTypeMap[index]);
-    }
-    activeEditor.setDecorations(isolatedRightBracketsDecorationTypes, rightBracketsDecorationTypes);
+  }
+
+  for (var index in divsDecorationTypes) {
+    activeEditor.setDecorations(
+      divsDecorationTypes[index],
+      divsDecorationTypeMap[index]
+    );
+  }
+  activeEditor.setDecorations(
+    isolatedRightBracketsDecorationTypes,
+    rightBracketsDecorationTypes
+  );
 }
 
 function deactivate() {}
 
 module.exports = {
-    activate: activate,
-    deactivate: deactivate,
+  activate: activate,
+  deactivate: deactivate
 };
